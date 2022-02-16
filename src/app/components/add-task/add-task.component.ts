3ec4e188e30c704daa620e6c4d4205caae9ccf09
Roshan Kharke <roshan_bt2k16@dtu.ac.in>
2022-02-16 14:01:06 +0530

    initial commit

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text: string | undefined;
+  day: string | undefined;
+  reminder: boolean = false;
+  showAddTask: boolean | undefined;
+  subscription: Subscription | undefined;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text || !this.day) {
+      alert('Please add a task & day/date!');
+      return;
+    }
+
+    const newTask: Task = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription | undefined;
+  constructor(private uiService: UiService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+}
